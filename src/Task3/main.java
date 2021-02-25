6adc8c20f60294ef974327ba52cf7f8af0e83ad2
FumeFedoraNB <fumefedoranb@localhost.localdomain>
2021-02-25 18:11:54 +0300

    Task 3 Fix (not full)

--- src/Task3/main.java
+++ src/Task3/main.java
@@ -9,26 +9,7 @@ import java.util.stream.Collectors;
 
 public class main {
     public static void main(String[] args) throws Exception {
-        ConcurrentHashMap certificationCosts = new ConcurrentHashMap<>();
-        certificationCosts.put("ABC", 1);
-        certificationCosts.put("BCD", 2);
-        certificationCosts.put("CDE", 3);
-        certificationCosts.put("DEF", 4);
-
-        Set concurrentSet = certificationCosts.keySet();
-
-        System.out.println("Before adding in Set: " + concurrentSet);
-        //concurrentSet.add("EFG");
-        System.out.println("After adding in Set: " + concurrentSet);
-
-        Set concurentHashSet = certificationCosts.newKeySet();
-        concurentHashSet.add("asd");
-        concurentHashSet.contains("asd");
-        concurentHashSet.remove("asd");
-        System.out.println("After addinmg into concurrent hash set: " + concurrentSet);
-
-        concurrentSet = certificationCosts.keySet(246);
-        concurrentSet.add("Sping enterprise");
+        
 
     }
 }
